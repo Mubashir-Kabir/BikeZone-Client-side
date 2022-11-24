41281b2f419498a0a8d4d9f6886b9ab204ba2f0f
Mubashir Kabir <97.m.kabir@gmail.com>
2022-11-24 07:14:36 +0600

    single product card updated

---
+++ src/components/ProductCard.js
@@ -0,0 +1,62 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../context/UserContext";
+
+const ProductCard = ({ item }) => {
+  const { user } = useContext(AuthContext);
+  const {
+    title, //------------
+    image, //---------
+    price,
+    condition,
+    seller, //-----------
+    number,
+    location,
+    info, //------------
+    purchaseYear,
+    sold,
+    postTime,
+  } = item;
+  if (sold) {
+    return <></>;
+  }
+  return (
+    <div className="card lg:card-side bg-base-100 shadow-xl">
+      <figure>
+        <img src={image} alt="Album" />
+      </figure>
+      <div className="card-body">
+        <h2 className="text-center text-3xl font-semibold">{title}</h2>
+        <p className="text-left mb-3">{info}</p>
+        <div className="flex flex-wrap gap-4 text-left">
+          <p>Original Price:${price.original}</p>
+          <p>Resale Price:${price.resale}</p>
+          <p>Condition: {condition}</p>
+          <p>Location: {location}</p>
+          <p>Purchase: {purchaseYear}</p>
+        </div>
+        <h3 className="text-2xl font-semibold">Posted By</h3>
+        <div className="flex justify-between">
+          <div className="flex space-x-4">
+            <div>
+              <img
+                src={user.photoURL}
+                alt=""
+                className="object-cover w-12 h-12 rounded-full bg-gray-500"
+              />
+            </div>
+            <div className="text-left">
+              <h4 className="font-bold">{user.displayName}</h4>
+              <span className="text-xs text-gray-600">{postTime}</span>
+              <p className="text-xs text-gray-600">{number}</p>
+            </div>
+          </div>
+        </div>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Book</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,9 +1,76 @@
 import React from "react";
+import ProductCard from "../components/ProductCard";
 
 const Products = () => {
+  const items = [
+    {
+      id: 1,
+      title: "Pulser NS 160cc",
+      price: {
+        original: 190000,
+        resale: 150000,
+      },
+      condition: "food",
+      seller: {
+        sellerID: 2, //seller name, pic hoile valo,seller varified kina,
+      },
+      number: "017723452345",
+      location: "Dhaka",
+      info: "this is good condition bike. nothing serious happened.i only hit the bike twice.but i repair imidiet after hitting.",
+      purchaseYear: "2018",
+      sold: false,
+      image:
+        "https://www.banglamotor.net/images/bajaj/bajaj-pulsar-ns160-img01.jpg",
+      postTime: "22-11-2022",
+    },
+    {
+      id: 2,
+      title: "Pulser NS 160cc",
+      price: {
+        original: 190000,
+        resale: 150000,
+      },
+      condition: "food",
+      seller: {
+        sellerID: 2, //seller name, pic hoile valo,seller varified kina,
+      },
+      number: "017723452345",
+      location: "Dhaka",
+      info: "this is good condition bike. nothing serious happened.i only hit the bike twice.but i repair imidiet after hitting.",
+      purchaseYear: "2018",
+      sold: false,
+      image:
+        "https://www.banglamotor.net/images/bajaj/bajaj-pulsar-ns160-img01.jpg",
+      postTime: "22-11-2022",
+    },
+    {
+      id: 3,
+      title: "Pulser NS 160cc",
+      price: {
+        original: 190000,
+        resale: 150000,
+      },
+      condition: "food",
+      seller: {
+        sellerID: 2, //seller name, pic hoile valo,seller varified kina,
+      },
+      number: "017723452345",
+      location: "Dhaka",
+      info: "this is good condition bike. nothing serious happened.i only hit the bike twice.but i repair imidiet after hitting.",
+      purchaseYear: "2018",
+      sold: false,
+      image:
+        "https://www.banglamotor.net/images/bajaj/bajaj-pulsar-ns160-img01.jpg",
+      postTime: "22-11-2022",
+    },
+  ];
   return (
-    <div>
-      <h1>this is products page</h1>
+    <div className="lg:mx-40 lg:my-10">
+      <div className="grid gap-8">
+        {items.map((item) => (
+          <ProductCard key={item.id} item={item}></ProductCard>
+        ))}
+      </div>
     </div>
   );
 };
